cc87d8cbf4c6318d82a9c2ba7a847ce6c8bc87c8
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-07-16 10:49:56 -0700

    WIP

--- lib/aa.rb
+++ lib/aa.rb
@@ -1,11 +1,12 @@
 require('pry')
 
 class Anagrams
-  def initialize(word1,word2)
+  attr_reader(:word1, :word2)
+  def initialize (word1)
     @word1 = word1
     @word2 = word2
   end
-  def letter_check
+  def letter_check (word1, word2)
     @word1.include?(@word2)
   end
 end
--- spec/aa_spec.rb
+++ spec/aa_spec.rb
@@ -5,8 +5,7 @@ describe('#anagrams') do
   it("it should check if word1 and word2 contain the same letters") do
     word1 = Anagrams.new("ruby")
     word2 = Anagrams.new("bury")
-    word1.letter_check(word2)
-    expect(word1.letter_check(word2)).to(eq('true'))
+    expect(word1.letter_check(word1,word2)).to(eq(true))
   end
 end
 
