a20bd676cfbc12f673381821a5375b4349df8f0c
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-07-16 09:30:47 -0700

    WIP; first test

--- lib/aa.rb
+++ lib/aa.rb
@@ -1,7 +1,22 @@
 require('pry')
 
 class Anagrams
-  def initialize (word)
-    @word = word
+  def initialize(word1,word2)
+    @word1 = word1
+    @word2 = word2
+  end
+  def letter_check
+    @word1.include?(@word2)
   end
 end
+
+
+
+
+
+
+
+
+  
+
+
--- spec/aa_spec.rb
+++ spec/aa_spec.rb
@@ -1,9 +1,23 @@
 require 'rspec'
 require 'aa'
 
-describe('anagrams') do
-  it() do
-    word = Anagrams.new(ruby)
-    expect(word.letter_check(word)).to(eq('answer'))
+describe('#anagrams') do
+  it("it should check if word1 and word2 contain the same letters") do
+    word1 = Anagrams.new("ruby")
+    word2 = Anagrams.new("bury")
+    word1.letter_check(word2)
+    expect(word1.letter_check(word2)).to(eq('true'))
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
