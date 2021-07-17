5c2588428980404c09b35a3bcde63c03c91070fe
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-07-16 17:01:46 -0700

    update tests

--- lib/aa.rb
+++ lib/aa.rb
@@ -5,8 +5,14 @@ class Anagrams
     @words = words
   end
   def anagram_check 
+    letter_array = []
     words_array = @words.split(" ")
+    words_array.each do |a|
+      letter_array.push(a.split(""))
+    end
+    letter_array
   end
+  
 end
 
 
--- spec/aa_spec.rb
+++ spec/aa_spec.rb
@@ -2,9 +2,13 @@ require 'rspec'
 require 'aa'
 
 describe('#anagrams') do
-  it("it returns an array of words") do
+  it("it returns an array which elements contains an array of letters") do
     words = Anagrams.new("ruby bury")
-    expect(words.anagram_check).to(eq(["ruby","bury"]))
+    expect(words.anagram_check).to(eq([["r", "u", "b", "y"], ["b", "u", "r", "y"]]))
+  end
+  it("it will compare each array of letters to determine if they equal") do
+    words = Anagrams.new("ruby bury")
+    expect(words.anagram_check).to(eq(true))
   end
 end
 
