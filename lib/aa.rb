fc27d209f30daf4d62a83391f480bf82ec6e8dfb
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-07-20 07:33:01 -0700

    create check_vowel test;fail

--- lib/aa.rb
+++ lib/aa.rb
@@ -1,23 +1,28 @@
 require('pry')
 
 class Anagrams
-  def initialize (words)
+  def initialize (word1, word2, words)
+    @word1 = word1.downcase
+    @word2 = word2
     @words = words
   end
-  def anagram_check 
+  
+  def anagram_check
     letter_array = []
     words_array = @words.split(" ")
     words_array.each do |a|
       letter_array.push(a.split(""))
     end
     letter_array
-    if letter_arra
   end
-
-
 end
 
-put letters into a key value hash.  then compare keys with includes
+
+
+
+
+
+# put letters into a key value hash.  then compare keys with includes
 
 
 
