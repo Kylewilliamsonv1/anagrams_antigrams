c4fc4400b2ab387d0eafb3e2640b501f5209fc32
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-07-22 14:04:41 -0700

    initializes 2 variables to compare

--- lib/aa.rb
+++ lib/aa.rb
@@ -1,20 +1,33 @@
 require('pry')
 
 class Anagrams
-  def initialize (words)
-    @words = words.downcase
+  def initialize (word1, word2)
+    @word1 = word1.downcase
+    @word2 = word2.downcase
   end
-
+# [[r,u,b,y],[b,u,r,y]
+# regex - gsub to remove punction when sorting
 
   def anagram_check
     letter_array = []
     anagrams = 0
-    words_array = @words.split(" ")
-    words_array.each do |word|
+    words_array1 = @word1.split("").sort
+    words_array2 = @word2.split("").sort
+binding.pry
+    # words_array.map.each_with_index { |k,v| k.}
+    words_array.each do |word| 
       letter_array.push(word.split("").sort)
     end
     letter_array
   end
+
+  # def anagram_check
+  #   anagram = []
+  #   words_array[0] = @words.split(" ") 
+
+  #   words_array.map { |i| i.count(i+1) }
+  #   anagram
+  # end
 
   # def anagram_check
 
--- spec/aa_spec.rb
+++ spec/aa_spec.rb
@@ -3,17 +3,17 @@ require 'aa'
 
 describe('#anagrams') do
   it("it will check to see if the inputted word contains a vowel") do
-    words = Anagrams.new("RUby bury")
-    expect(words.vowel_check).to(eq(true))
+    word = Anagrams.new("RUby", "bury")
+    expect(word.vowel_check).to(eq(true))
   end
   it("it returns an array which elements contains an array of letters") do
-    words = Anagrams.new("ruby bury")
-    expect(words.anagram_check).to(eq([["r", "u", "b", "y"], ["b", "u", "r", "y"]]))
+    word = Anagrams.new("ruby", "bury")
+    expect(word.anagram_check).to(eq([["b", "u", "r", "y"], ["b", "u", "r", "y"]]))
   end
   it("it will compare each array of letters to determine if they equal") do
-    words = Anagrams.new("ruby bury")
+    word = Anagrams.new("ruby", "bury")
 
-    expect(words.anagram_check).to(eq(true))
+    expect(word.anagram_check).to(eq(true))
   end
 end
 
