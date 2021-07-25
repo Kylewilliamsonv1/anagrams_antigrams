416cc2862d678b8f62577286acc753c669b75467
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-07-24 21:31:20 -0700

    anagram_check; test pass/fail

--- lib/aa.rb
+++ lib/aa.rb
@@ -2,51 +2,34 @@ require('pry')
 
 class Anagrams
   def initialize (word1, word2)
-    @word1 = word1.downcase
-    @word2 = word2.downcase
+    @word1 = word1.downcase.gsub(/[^A-Za-z0-9\s]/i, '')
+    @word2 = word2.downcase.gsub(/[^A-Za-z0-9\s]/i, '')
   end
-# [[r,u,b,y],[b,u,r,y]
-# regex - gsub to remove punction when sorting
+
+  def are_words
+    vowel_count = @word1.count('aeiou')
+    if vowel_count >= 1
+      true
+    end
+  end
 
   def anagram_check
-    letter_array = []
-    anagrams = 0
+    letter_match = 0
     words_array1 = @word1.split("").sort
     words_array2 = @word2.split("").sort
-binding.pry
-    # words_array.map.each_with_index { |k,v| k.}
-    words_array.each do |word| 
-      letter_array.push(word.split("").sort)
+    words_array1.each do |w1|
+      words_array2.each do |w2|
+        if w1 == w2
+        letter_match += 1
+        end
+      end
     end
-    letter_array
+    if letter_match == words_array1.length
+      "These words are anagrams."
+    end
   end
-
-  # def anagram_check
-  #   anagram = []
-  #   words_array[0] = @words.split(" ") 
-
-  #   words_array.map { |i| i.count(i+1) }
-  #   anagram
-  # end
-
-  # def anagram_check
-
-  #   hash = Hash.new
-  #   ("ruby bury").each_with_index { |item, index|
-  #     hash[item] = index
-  #   }
-  #   hash
-  # binding.pry
-  # end
-
 end
 
-
-
-
-
-
-# put letters into a key value hash.  then compare keys with includes
 
 
 
--- spec/aa_spec.rb
+++ spec/aa_spec.rb
@@ -4,16 +4,12 @@ require 'aa'
 describe('#anagrams') do
   it("it will check to see if the inputted word contains a vowel") do
     word = Anagrams.new("RUby", "bury")
-    expect(word.vowel_check).to(eq(true))
+    expect(word.are_words).to(eq(true))
   end
-  it("it returns an array which elements contains an array of letters") do
-    word = Anagrams.new("ruby", "bury")
-    expect(word.anagram_check).to(eq([["b", "u", "r", "y"], ["b", "u", "r", "y"]]))
-  end
-  it("it will compare each array of letters to determine if they equal") do
-    word = Anagrams.new("ruby", "bury")
 
-    expect(word.anagram_check).to(eq(true))
+  it("it compares two words to see if they are anagrams.") do
+    word = Anagrams.new("ruby", "bury")
+    expect(word.anagram_check).to(eq("These words are anagrams."))
   end
 end
 
